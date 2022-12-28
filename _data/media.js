9c723a54791e599995df2e5860334b0515e641a4
Alexander Wunschik <alex@wunschik.net>
2022-12-28 21:58:33 +0100

    feat: add media

--- _data/media.js
+++ _data/media.js
@@ -4,5 +4,5 @@ const { archiveCache, getFilesFromDirectory } = require('../helpers.js');
 module.exports = async function() {
 	return getFilesFromDirectory(
 		path.join(archiveCache, 'media_attachments')
-	);
+	).map(attachment => `/${attachment}`);
 };
