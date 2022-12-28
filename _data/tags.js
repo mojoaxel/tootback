bd558904f3ff1a0b56c02407a26cb77b76cab598
Alexander Wunschik <alex@wunschik.net>
2022-12-28 21:02:43 +0100

    feat: add tags

--- _data/tags.js
+++ _data/tags.js
@@ -7,9 +7,30 @@ module.exports = async function() {
 	const outboxFile = actor.outbox;
 	const dataRaw = await fs.readFile(`${archiveCache}/${outboxFile}`, 'utf8');
 	const data = JSON.parse(dataRaw);
-	return data.orderedItems
-		.map(item => item.object)
-		.filter(object => object.tag && object.tag.length > 0)
-		.flatMap(object => object.tag)
-		.filter((tag, index, array) => array.indexOf(tag) === index)
+
+	const tagsMap = data.orderedItems
+			.map(item => item.object)
+			.filter(object => object.tag && object.tag.length > 0)
+			.flatMap(object => object.tag)
+			.filter(tag => tag.type === 'Hashtag')
+			.reduce((tagSet, tag) => {
+				const count = tagSet.has(tag.name) ? tagSet.get(tag.name).count + 1 : 1;
+				tagSet.set(tag.name, {
+					...tag,
+					count
+				});
+				return tagSet;
+			}, new Map());
+	let tags = Array.from(tagsMap.values())
+		.sort((a, b) => a.name.localeCompare(b.name));
+
+	const tagsMaxCount = tags.reduce((max, tag) => Math.max(max, tag.count), 0);
+
+	// Add scaling between 0..1
+	tags = tags.map(tag => ({
+		...tag,
+		scaling: Math.round(tag.count / tagsMaxCount)
+	}));
+
+	return tags;
 };
